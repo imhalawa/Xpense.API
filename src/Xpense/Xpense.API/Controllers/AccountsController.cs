508f14c492387d761f6a1183ce7d69b7cfebf3c6
Mohamed Halawa <imhalawa@outlook.com>
2023-08-30 18:57:41 +0300

    Revert this commit, for syncing purpose only

---
+++ src/Xpense/Xpense.API/Controllers/AccountsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Xpense.API.Controllers
+{
+    [Route("api/accounts")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        [HttpGet("{accoundId:int}")]
+        public async Task<ActionResult<string>> GetAccount(int accountId)
+        {
+            return Ok(Task.FromResult("Sample Account"));
+        }
+    }
+}
--- src/Xpense/Xpense.API/Program.cs
+++ src/Xpense/Xpense.API/Program.cs
@@ -1,32 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseRouting();
+app.MapControllers();
 
-var summaries = new[]
+if (app.Environment.IsDevelopment())
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("","v1");
+        options.RoutePrefix=String.Empty;
+    });
+}
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
