71b4418b77f20b81ca765809f6d036d31c14c565
Mohamed Halawa <imhalawa@outlook.com>
2023-10-07 02:34:18 +0300

    XPNS fix: Remove Redundant Properties

--- src/Xpense/Xpense.API/Data/Models/CurrencyExchangeRate.cs
+++ src/Xpense/Xpense.API/Data/Models/CurrencyExchangeRate.cs
@@ -2,8 +2,6 @@
 
 public class CurrencyExchangeRate : BaseEntity
 {
-    public Currency FromCurrency { get; set; }
-    public Currency ToCurrency { get; set; }
     public decimal Rate { get; set; }
     public int PrincipalCurrencyId { get; set; }
     public Currency PrincipalCurrency { get; set; }
