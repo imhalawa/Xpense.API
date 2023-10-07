b7186060e466e9280756f1871abce0795fa673d2
Mohamed Halawa <imhalawa@outlook.com>
2023-10-07 04:13:32 +0300

    XPNS#11 feat: Added A UnitTest project with two tests Red,Green

---
+++ src/Xpense/Xpense.API.UnitTests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Xpense.API.UnitTests.Controllers
+{
+    [TestFixture]
+    public class AccountControllerTests
+    {
+        [Test]
+        public void RedTest()
+        {
+            Assert.AreEqual(1,2);
+        }
+
+        [Test]
+        public void GreenTest()
+        {
+            Assert.AreEqual(2, 2);
+        }
+    }
+}
