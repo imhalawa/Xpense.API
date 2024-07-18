f2da1e266f513bda0a0e06dc2fa020236e33691f
Mohamed Halawa <imhalawa@outlook.com>
2024-07-19 01:10:01 +0200

    feat: include createdOn & lastUpdated in Merchant Response

--- src/Xpense/Xpense.API/Models/Responses/MerchantResponse.cs
+++ src/Xpense/Xpense.API/Models/Responses/MerchantResponse.cs
@@ -1,15 +1,27 @@
-﻿using Xpense.Services.Entities;
+﻿using System;
+using Xpense.Services.Entities;
 
 namespace Xpense.API.Models.Responses
 {
     public class MerchantResponse(
         int id,
-        string label)
+        string label,
+        long? createdOn,
+        long? lastUpdated)
     {
         public int Id { get; set; } = id;
         public string Label { get; set; } = label;
         public bool Create { get; set; } = false;
+        public long? CreatedOn { get; set; } = createdOn;
+        public long? LastUpdated { get; set; } = lastUpdated;
 
-        public static MerchantResponse Of(Merchant merchant) => new(merchant.Id, merchant.Label);
+        public static MerchantResponse Of(Merchant merchant)
+        {
+            var createdOn = new DateTimeOffset(merchant.CreatedOn).ToUnixTimeSeconds();
+            long? lastUpdated = merchant.LastUpdated.HasValue
+                ? new DateTimeOffset(merchant.LastUpdated.Value).ToUnixTimeSeconds()
+                : null;
+            return new MerchantResponse(merchant.Id, merchant.Label, createdOn, lastUpdated);
+        }
     }
 }
