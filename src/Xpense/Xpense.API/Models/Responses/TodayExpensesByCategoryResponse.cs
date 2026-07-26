269894db66c3e6233e53a0a06cf9f9906c482a2d
Mohamed Halawa <imhalawa@outlook.com>
2026-07-26 17:09:19 +0200

    fix(api): return direct v1 analytics payload

--- src/Xpense/Xpense.API/Models/Responses/TodayExpensesByCategoryResponse.cs
+++ src/Xpense/Xpense.API/Models/Responses/TodayExpensesByCategoryResponse.cs
@@ -1,23 +1,30 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Xpense.Services.Models;
-using Xpense.Services.ValueObjects;
 
 namespace Xpense.API.Models.Responses
 {
-    public class TodayExpensesByCategoryResponse(IEnumerable<ExpensesByCategoryResponse> expenses, Money total)
+    public class TodayExpensesByCategoryResponse(IEnumerable<ExpensesByCategoryResponse> expenses, V1AnalyticsMoneyResponse total)
     {
         public IEnumerable<ExpensesByCategoryResponse> Expenses { get; set; } = expenses;
-        public Money Total { get; set; } = total;
+        public V1AnalyticsMoneyResponse Total { get; set; } = total;
 
         public static TodayExpensesByCategoryResponse Of(TodayExpensesByCategory expensesByCategory)
         {
             var expenses = expensesByCategory?
                 .Expenses?
                 .Select(ExpensesByCategoryResponse.Of);
-            var total = expensesByCategory?.Total ?? Money.Zero;
+            var total = V1AnalyticsMoneyResponse.Of(expensesByCategory?.Total);
 
             return new TodayExpensesByCategoryResponse(expenses, total);
+        }
+    }
+
+    public sealed record V1AnalyticsMoneyResponse(long Cents, string Currency)
+    {
+        public static V1AnalyticsMoneyResponse Of(Xpense.Services.ValueObjects.Money money)
+        {
+            return new V1AnalyticsMoneyResponse(money?.Cents ?? 0, money?.Currency.ToString() ?? "EUR");
         }
     }
 }
