20e64d6b0b83b16e5f26678fbd60c9e9c54675aa
Mohamed Halawa <imhalawa@outlook.com>
2026-07-26 14:27:27 +0200

    feat(api): add unified v1 transaction creation

---
+++ src/Xpense/Xpense.Tests/Unit/CreateTransactionRequestTests.cs
@@ -0,0 +1,76 @@
+using FluentAssertions;
+using Xpense.API.Models.Requests;
+using Xpense.Services.Enums;
+
+namespace Xpense.Tests.Unit;
+
+[TestFixture]
+public class CreateTransactionRequestTests
+{
+    [Test]
+    public void Income_maps_public_values_to_a_deposit_command()
+    {
+        var occurredAt = new DateTimeOffset(2026, 7, 26, 9, 30, 0, TimeSpan.Zero);
+        var request = new CreateTransactionRequest
+        {
+            Type = "income",
+            Amount = new TransactionMoneyRequest(1234, "EUR"),
+            AccountNumber = "1234567890",
+            CategoryId = 7,
+            Merchant = new TransactionMerchantRequest(4, "Employer", false),
+            Tags = [new TransactionTagRequest(2, "salary", false)],
+            OccurredAt = occurredAt
+        };
+
+        request.TryGetKind(out var kind).Should().BeTrue();
+        kind.Should().Be(TransactionKind.Income);
+
+        var command = request.ToDepositCommand();
+        command.Amount.Cents.Should().Be(1234);
+        command.Amount.Currency.Should().Be(Currency.EUR);
+        command.AccountNumber.Should().Be("1234567890");
+        command.CategoryId.Should().Be(7);
+        command.Merchant.Id.Should().Be(4);
+        command.Merchant.Label.Should().Be("Employer");
+        command.Merchant.Create.Should().BeFalse();
+        command.Tags.Should().ContainSingle();
+        command.Tags![0].Id.Should().Be(2);
+        command.Tags[0].Label.Should().Be("salary");
+        command.CreatedOn.Should().Be(occurredAt.ToUnixTimeSeconds());
+    }
+
+    [Test]
+    public void Expense_maps_optional_account_and_timestamp_to_a_withdraw_command()
+    {
+        var request = new CreateTransactionRequest
+        {
+            Type = "expense",
+            Amount = new TransactionMoneyRequest(99, "USD"),
+            CategoryId = 3,
+            Merchant = new TransactionMerchantRequest(null, "Coffee Shop", true)
+        };
+
+        request.TryGetKind(out var kind).Should().BeTrue();
+        kind.Should().Be(TransactionKind.Expense);
+
+        var command = request.ToWithdrawCommand();
+        command.Amount.Cents.Should().Be(99);
+        command.Amount.Currency.Should().Be(Currency.USD);
+        command.AccountNumber.Should().BeNull();
+        command.CategoryId.Should().Be(3);
+        command.Merchant.Id.Should().BeNull();
+        command.Merchant.Label.Should().Be("Coffee Shop");
+        command.Merchant.Create.Should().BeTrue();
+        command.Tags.Should().BeNull();
+        command.CreatedOn.Should().BeNull();
+    }
+
+    [TestCase("transfer")]
+    [TestCase("refund")]
+    public void Unsupported_type_does_not_classify_as_a_transaction_kind(string type)
+    {
+        var request = new CreateTransactionRequest { Type = type };
+
+        request.TryGetKind(out _).Should().BeFalse();
+    }
+}
